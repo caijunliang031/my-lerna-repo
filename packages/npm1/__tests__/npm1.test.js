cac01d94f42f9ac7deb8faee28d9694041a8057f
caijunliang <caijunliang@bytedance.com>
2020-06-09 17:53:48 +0800

    npm1

---
+++ packages/npm1/__tests__/npm1.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const npm1 = require('..');
+
+describe('npm1', () => {
+    it('needs tests');
+});
---
+++ packages/npm1/lib/npm1.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = npm1;
+
+function npm1() {
+    // TODO
+}

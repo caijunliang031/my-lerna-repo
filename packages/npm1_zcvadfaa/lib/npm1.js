f9ad9449e268e531c4b6fd435d9e39cedac7ea32
caijunliang <caijunliang@bytedance.com>
2020-12-11 15:09:38 +0800

    feat: test from

--- packages/npm1_zcvadfaa/lib/npm1.js
+++ packages/npm1_zcvadfaa/lib/npm1.js
@@ -4,5 +4,5 @@ module.exports = npm1;
 
 function npm1() {
     // TODO
-    console.log("c111h11ange4444   12lal111111111alafasdfl31111123");
+    console.log("c111h11ange44555544   12lal111111111alafasdfl31111123");
 }
